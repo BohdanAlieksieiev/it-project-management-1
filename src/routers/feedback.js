bc977dc547e58dbe6a3cafde40b9c9ad03583097
BohdanAlieksieiev <bohdan.alieks@gmail.com>
2021-10-02 16:22:56 +0300

    try to fix cors

--- src/routers/feedback.js
+++ src/routers/feedback.js
@@ -3,12 +3,18 @@ const Feedback = require('../model/feedback')
 const router = express.Router()
 
 router.get('/', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
     res.send('Hello world');
 });
 
 router.post('/form', async function (req, res) {
     const feedback = new Feedback(req.body)
     console.log(req.body)
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+
     feedback.save().then(() => {
         res.send(feedback)
     }).catch((err) => {
