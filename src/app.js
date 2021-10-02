6e589b06f1b783cb7650c6095379926a3d236eab
BohdanAlieksieiev <bohdan.alieks@gmail.com>
2021-10-02 16:20:35 +0300

    xfhcgjvkbhlj;nkm

--- src/app.js
+++ src/app.js
@@ -1,29 +1,20 @@
-// const request = require("request")
-// const hbs = require('hbs')
-
 const express = require('express')
-const fs = require('fs');
 var cors = require('cors')
-// const mongoose = require('mongoose')
-const validator = require('validator')
 const jsonParse = express.json()
-// MODEL
 require('./db/mongoose')
 require('./model/feedback')
-
-const Feedback = require('mongoose').model('Feedback')
 const FeedbackRouter = require('./routers/feedback')
 
 let app = express();
 
 const middleware = [
     cors(),
-    express.json(),
+    jsonParse,
     FeedbackRouter
 ]
 
 app.use(middleware);
 
-app.listen(3000, () => { // запуск на порті
+app.listen(3000, () => {
     console.log("Example app listening on port 3000");
 });
--- src/routers/feedback.js
+++ src/routers/feedback.js
@@ -2,15 +2,15 @@ const express = require('express')
 const Feedback = require('../model/feedback')
 const router = express.Router()
 
-router.get('/', function (req, res, next) {
-    res.status(200).send('Hello world');
+router.get('/', function (req, res) {
+    res.send('Hello world');
 });
 
-router.post('/form', async function (req, res, next) {
+router.post('/form', async function (req, res) {
     const feedback = new Feedback(req.body)
     console.log(req.body)
     feedback.save().then(() => {
-        res.status(201).send(feedback)
+        res.send(feedback)
     }).catch((err) => {
         res.status(401).send(err.message)
     })
