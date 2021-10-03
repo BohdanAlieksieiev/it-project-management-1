b0b983a8fcc643c977db14a85e0ee6e9498c3079
BohdanAlieksieiev <bohdan.alieks@gmail.com>
2021-10-03 13:26:08 +0300

    adgsfg

--- src/app.js
+++ src/app.js
@@ -15,6 +15,6 @@ const middleware = [
 
 app.use(middleware);
 
-app.listen(3000, () => {
-    console.log("Example app listening on port 3000");
+app.listen(8080, () => {
+    console.log("Example app listening on port 8080");
 });
