551747c7862e20774ea657e068d92502846678fc
BohdanAlieksieiev <bohdan.alieks@gmail.com>
2021-10-03 13:41:41 +0300

    fix modelu

--- src/model/feedback.js
+++ src/model/feedback.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 let FeedbackSchema = new mongoose.Schema({
-    text:{
+    name:{
         type:String,
+        required: true
+    },
+    email:{
+        type:String,
+        required: true
     },
 })
 
